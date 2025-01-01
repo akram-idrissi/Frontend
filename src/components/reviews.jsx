be825829ea22ce77390b57055b6ea5344c4dc005
Akram Idrissi <akram.mo.gb@gmail.com>
2025-01-01 18:12:49 +0100

    Changed reviews

--- src/components/reviews.jsx
+++ src/components/reviews.jsx
@@ -4,28 +4,21 @@ const reviews = [
   {
     id: 1,
     rating: 5,
-    content: `
-      <p>This icon pack is just what I need for my latest project. There's an icon for just about anything I could ever need. Love the playful look!</p>
-    `,
-    date: 'July 16, 2021',
-    datetime: '2021-07-16',
-    author: 'Emily Selman',
-    avatarSrc:
-      'https://images.unsplash.com/photo-1502685104226-ee32379fefbe?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80',
+    content: 'Obsessed with these jeans! The high waist and stretch are just perfect, and they go with everything I own!',
+    date: 'July 16, 2024',
+    datetime: '2024-07-16',
+    author: 'Ghazi Sofia',
+    avatarSrc: '/testimonials/gs.jpg',
   },
   {
     id: 2,
     rating: 5,
-    content: `
-      <p>Blown away by how polished this icon pack is. Everything looks so consistent and each SVG is optimized out of the box so I can use it directly with confidence. It would take me several hours to create a single icon this good, so it's a steal at this price.</p>
-    `,
-    date: 'July 12, 2021',
-    datetime: '2021-07-12',
-    author: 'Hector Gibbons',
-    avatarSrc:
-      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80',
+    content: 'Loving my new sweater from this brand! It’s super cozy, fits perfectly, and the color is exactly as shown online!',
+    date: 'July 12, 2024',
+    datetime: '2024-07-12',
+    author: 'Sofia Berbich',
+    avatarSrc: '/testimonials/sb.jpg',
   },
-  // More reviews...
 ]
 
 function classNames(...classes) {
