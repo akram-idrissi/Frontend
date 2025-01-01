0a146e776ec177f12974c9a0af77b310f1b87cf6
Akram Idrissi <akram.mo.gb@gmail.com>
2025-01-01 22:33:43 +0100

    Removed onchange

--- src/app/auth/signin/page.jsx
+++ src/app/auth/signin/page.jsx
@@ -41,7 +41,6 @@ export default function Signin() {
                                     id="username"
                                     name="username"
                                     type="text"
-                                    /* onChange={(event) => { setErrors({ ...errors, username: "" }); setUsername(event.target.value) }} */
                                     autoComplete="username"
                                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6"
                                 />
@@ -69,7 +68,6 @@ export default function Signin() {
                                     id="password"
                                     name="password"
                                     type="password"
-                                    /* onChange={(event) => { setErrors({ ...errors, password: "" }); setPassword(event.target.value) }} */
                                     autoComplete="current-password"
                                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6"
                                 />
