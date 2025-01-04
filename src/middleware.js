de4f56c36ed8f63845c7d80fa67d9a4f869110ce
Akram Idrissi <akram.mo.gb@gmail.com>
2025-01-04 11:55:37 +0100

    Updated middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,11 @@
 import { NextResponse } from 'next/server'
- 
-export function middleware(request) {    
-    if (request.cookies.has('user'))
-        return NextResponse.redirect(new URL('/', request.url))
+
+export function middleware(request) {
+  const user = request.cookies.get('user')
+  if (user)
+    return NextResponse.redirect(new URL('/', request.url))
 }
- 
+
 export const config = {
-  matcher: '/auth/signin/',
+  matcher: '/auth/:path*',
 }
