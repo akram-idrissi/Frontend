6b05e8e7f62e29354beba5da45c8792e224e8473
Akram Idrissi <akram.mo.gb@gmail.com>
2025-01-04 17:15:23 +0100

    Revert changes

--- src/app/categories/[name]/api.js
+++ src/app/categories/[name]/api.js
@@ -1,17 +1,17 @@
-const getCategoryProducts = async (category) => {
+import axios from "axios";
 
-  const backendURL = process.env.NEXT_PUBLIC_BACKEND_URL;
-  const url = `${backendURL}/api/categories/${category}?page=1`
-  const response = await fetch(url, { method: "GET" }, { cache: 'no-store' });
 
-  if (!response.ok) {
-    return {
-      success: false,
-      status: response.status,
-      message: 'An unexpected error occurred',
-    }
+const backendURL = process.env.NEXT_PUBLIC_BACKEND_URL;
+export const getProductsByCategory = async (category, page=1, size=5) => {
+  const result = {loading: true, error: null, data: null};
+
+  try {
+    const response = await axios.get(`${backendURL}/api/categories/${category}?page=${page}`);
+    result.data = response.data;
+  } catch (error) {
+    result.error = error.message || "An error occurred while fetching category products";
+  } finally {
+    result.loading = false;
   }
-  return await response.json();
-}
-
-export default getCategoryProducts;
+  return result;
+};
