01cd301d51e32d269477ef49ad7802255932540c
Akram Idrissi <akram.mo.gb@gmail.com>
2025-01-04 10:24:40 +0100

    Added safety checks

--- src/app/auth/signin/page.jsx
+++ src/app/auth/signin/page.jsx
@@ -45,7 +45,7 @@ export default function Signin() {
                                     type="text"
                                     required
                                     placeholder="jone doe"
-                                    defaultValue={state.inputs?.username}
+                                    defaultValue={state?.inputs?.username}
                                     autoComplete="username"
                                     className={` ${state?.errors?.username ? 'border-red-500' : ''} block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6`}
                                 />
@@ -74,7 +74,7 @@ export default function Signin() {
                                     name="password"
                                     type="password"
                                     required
-                                    defaultValue={state.inputs?.password}
+                                    defaultValue={state?.inputs?.password}
                                     autoComplete="current-password"
                                     className={` ${state?.errors?.password ? 'border-red-500' : ''} block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-black sm:text-sm sm:leading-6`}
                                 />
