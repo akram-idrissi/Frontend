d71574a62fdf645ca83d67098de3842aa5e8747c
Akram Idrissi <akram.mo.gb@gmail.com>
2025-01-05 23:29:12 +0100

    Handling unexpected errors

---
+++ src/app/categories/[name]/error.jsx
@@ -0,0 +1,21 @@
+'use client'
+
+import Footer from "@/components/footer"
+import Navbar from "@/components/navbar"
+
+
+export default function Error({ error, reset }) {
+    return (
+        <div className="bg-white">
+            <Navbar sticky={true} />
+            <div className="mx-auto max-w-7xl my-10 p-6 lg:px-8">
+                <h1 className="text-4xl font-bold">Something went wrong!</h1>
+                <button className="border px-4 py-2 mt-4 border-black rounded-md font-medium"
+                    onClick={() => reset()}>
+                    Try again
+                </button>
+            </div>
+            <Footer />
+        </div>
+    )
+}
