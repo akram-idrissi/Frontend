22a381c3bad297058f58d578f111dec1212bbca7
Akram Idrissi <akram.mo.gb@gmail.com>
2025-01-08 09:28:54 +0100

    Made new api functions

--- src/app/products/[name]/api.js
+++ src/app/products/[name]/api.js
@@ -1,32 +1,21 @@
-import axios from "axios";
-
-
-const backendURL = process.env.NEXT_PUBLIC_BACKEND_URL;
-
-export const getProductById = async (id) => {
-  const result = {loading: true, error: null, data: null};
+export const getProduct = async (id) => {
 
   try {
-    const response = await axios.get(`${backendURL}/api/products/${id}`);
-    result.data = response.data;
+    const url = `${backendURL}/api/products/${id}`;
+    const response = await fetch(url, {cache: "no-store", method: "GET"});
+    return await response.json();
   } catch (error) {
-    result.error = error.message || "An error occurred while fetching the product";
-  } finally {
-    result.loading = false;
+    return error.message || "An error occurred while fetching the product";
   }
-  return result;
 };
 
-export const getSimilarProducts = async (category, id) => {
-  const result = {loading: true, error: null, data: null};
+export const getRelatedProducts = async (category, id) => {
 
   try {
-    const response = await axios.get(`${backendURL}/api/similar-products/${category}/${id}`);
-    result.data = response.data;
+    const url = `${backendURL}/api/similar-products/${category}/${id}`;
+    const response = await fetch(url, {cache: "no-cache", method: "GET"});
+    return await response.json();
   } catch (error) {
-    result.error = error.message || "An error occurred while fetching similar products";
-  } finally {
-    result.loading = false;
+    return error.message || "An error occurred while fetching related product";
   }
-  return result;
 };
