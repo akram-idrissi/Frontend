967761afcd36ff76078850cc9ac3c41db650bcfc
Akram Idrissi <akram.mo.gb@gmail.com>
2025-01-08 22:23:02 +0100

    Updated middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,26 @@
-import { NextResponse } from 'next/server'
+import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-  const user = request.cookies.get('user')
-  if (user)
-    return NextResponse.redirect(new URL('/', request.url))
+  const { pathname } = request.nextUrl;
+  const user = request.cookies.get('user');
+
+  if (pathname.startsWith('/auth')) {
+    if (user) {
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+  } else if (pathname === '/checkout') {
+    if (!user) {
+      return NextResponse.redirect(new URL(`/auth/signin?_next=cart`, request.url));
+    }
+    else if (pathname === '/orders') {
+      if (!user)
+        return NextResponse.redirect(new URL(`/`, request.url));
+    }
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: '/auth/:path*',
-}
+  matcher: ['/auth/:path*', '/checkout', '/orders'],
+};
