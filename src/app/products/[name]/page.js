5bb3a2b754005bb010cb061e4cef52b046e40e5d
Akram Idrissi <akram.mo.gb@gmail.com>
2025-01-08 09:28:06 +0100

    Made it server comp

--- src/app/products/[name]/page.js
+++ src/app/products/[name]/page.js
@@ -1,52 +1,31 @@
-'use client'
-
-import { useState, useEffect } from 'react'
-import { useParams, useSearchParams } from 'next/navigation';
-
+import Stars from '@/components/stars';
 import Navbar from '@/components/navbar'
-import Reviews from '@/components/reviews'
 import Footer from '@/components/footer'
-import { getProductById } from './api';
+import Reviews from '@/components/reviews'
+import Breadcrumb from '@/components/breadcrumb';
 
+import { getProduct, getRelatedProducts } from './api';
 
-import Sizes from './sizes';
 import Images from './images';
 import Details from './details';
 import Policies from './policies';
-import AddToCart from './addToCart';
-import Stars from '@/components/stars';
+import ClientWrapper from './wrapper';
 import RelatedProducts from './relatedProducts';
-import Breadcrumb from '../../../components/breadcrumb';
 
 
 const categoriesURI = "/categories";
 
 
-export default function ProductPage() {
-  const id = useParams()["name"];
-  const [error, setError] = useState("");
-  const [product, setProduct] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [selectedSize, setSelectedSize] = useState('');
+export default async function ProductPage({ params, searchParams }) {
 
-  const searchParams = useSearchParams();
-  const category = searchParams.get('category');
+  const id = params.name;
+  const category = searchParams.category;
 
-  useEffect(() => {
-    const fetchProduct = async (id) => {
-        const result = await getProductById(id);
-        setError(result.error);
-        setProduct(result.data);
-        setLoading(result.loading);
-    };
-
-    fetchProduct(id);
-  }, [id]);
-
-
+  const product = await getProduct(id);
+  const relatedProducts = await getRelatedProducts(category, id);
 
   const breadcrumbLinks = [
-    { 'href': `${categoriesURI}/${product ? product.category : ""}`, 'name': product ? product.category : "" },
+    { 'href': `${categoriesURI}/${category}`, 'name': category },
     { 'href': '', 'name': product ? product.title : "" }
   ]
 
@@ -60,45 +39,38 @@ export default function ProductPage() {
         <div className="mx-auto max-w-2xl lg:max-w-none">
           {/* Product */}
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
-            
+
             {/* Product info */}
-            {error && <h1 className='text-xl'>error</h1>}
-            {loading ? <h1 className='text-4xl'>Loading the product</h1> :
-            <> 
-              {/* Image gallery */}
-              <Images images={product.image_srcs} />
 
-              <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
-                <h1 className="text-3xl font-bold tracking-tight text-gray-900">{product.title}</h1>
+            {/* Image gallery */}
+            <Images images={product.image_srcs} />
 
-                <div className="mt-3">
-                  <h2 className="sr-only">Product information</h2>
-                  <p className="text-3xl tracking-tight text-gray-900">150 MAD</p>
-                </div>
+            <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+              <h1 className="text-3xl font-bold tracking-tight text-gray-900">{product.title}</h1>
 
-                {/* Reviews */}
-                <div className="flex items-center mt-3">
-                  <Stars numbers={[0, 1, 2, 3, 4]} />
-                </div>
-
-                {/* Size picker */}
-                <Sizes sizes={product.sizes} selectedSize={selectedSize} setSelectedSize={setSelectedSize}/>
-
-                {/* Add to cart */}
-                <AddToCart product={product} selectedSize={selectedSize} />
-
-                {/* Product details */}
-                <Details />
-
-                {/* Policies */}
-                <Policies />
+              <div className="mt-3">
+                <h2 className="sr-only">Product information</h2>
+                <p className="text-3xl tracking-tight text-gray-900">150 MAD</p>
               </div>
-            </>
-            }
+
+              {/* Reviews */}
+              <div className="flex items-center mt-3">
+                <Stars numbers={[0, 1, 2, 3, 4]} />
+              </div>
+
+              {/* Sizes and Add to cart */}
+              <ClientWrapper product={product} />
+
+              {/* Product details */}
+              <Details />
+
+              {/* Policies */}
+              <Policies />
+            </div>
           </div>
 
           <Reviews />
-          <RelatedProducts category={category} productID={id} />
+          <RelatedProducts products={relatedProducts.products} />
         </div>
       </main>
 
