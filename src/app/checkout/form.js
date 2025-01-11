c6db54126c7279945848eee1785006e099222d20
Akram Idrissi <akram.mo.gb@gmail.com>
2025-01-11 17:36:36 +0100

    Updated form comp

--- src/app/checkout/form.js
+++ src/app/checkout/form.js
@@ -1,44 +1,30 @@
 'use client'
 
-import { useState } from 'react';
-import { checkout } from './api';
-import Loader from '@/common/loader';
-import ErrorAlert from '@/common/error-alert';
 import SuccessAlert from '@/common/sucess-alert';
 import { LockClosedIcon } from '@heroicons/react/20/solid'
-import { addOrder } from '../orders/api';
-import { clearCart } from '../cart/cart';
 
+import { useFormState } from 'react-dom';
+import { checkoutAction } from './actions';
+import { FormButton } from './form-button';
+
+
+const initialState = {
+  message: null,
+  errors: {},
+};
 
 export default function Form() {
 
-  const [error, setError] = useState("");
-  const [payed, setPayed] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  const handleFormSubmission = async (event) => {
-    event.preventDefault();
-
-    const result = await checkout({});
-    setLoading(result.loading);
-    setPayed(result.data ? true : false);
-    setError(result.error);
-    const user = JSON.parse(localStorage.getItem("user"));
-    await addOrder({email: user.email, price: 145, id: user.id, createdAt: "", status: ""});
-    clearCart();
-  }
+  const [state, formaAction] = useFormState(checkoutAction, initialState);
 
   return (
     <section
       aria-labelledby="payment-heading"
       className="flex-auto overflow-y-auto px-4 pb-16 pt-12 sm:px-6 sm:pt-16 lg:px-8 lg:pb-24 lg:pt-0"
     >
-      {loading && <Loader />}
-      {error && <ErrorAlert subject='Payment Error' object='Payment failed' /> }
-      {payed && <SuccessAlert subject='Payment Successful' object='' /> }
-
+      {state?.success && <SuccessAlert subject='Payment Successful' object='' />}
       <div className="mx-auto max-w-lg">
-        <form className="mt-6" onSubmit={(event) => handleFormSubmission(event) } >
+        <form className="mt-6" action={formaAction} >
           <div className="grid grid-cols-12 gap-x-4 gap-y-6">
             <div className="col-span-full">
               <label htmlFor="email-address" className="block text-sm font-medium text-gray-700">
@@ -50,9 +36,11 @@ export default function Form() {
                   name="email-address"
                   type="email"
                   autoComplete="email"
+                  defaultValue={state?.inputs?.email}
                   className="block w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black sm:text-sm"
                 />
               </div>
+              {state?.errors?.email && <p className="text-xs text-red-500">{state?.errors?.email.length > 1 ? state?.errors?.email[1] : state?.errors?.email[0]}</p>}
             </div>
 
             <div className="col-span-full">
@@ -64,10 +52,12 @@ export default function Form() {
                   id="card-number"
                   name="card-number"
                   type="text"
+                  defaultValue={state?.inputs?.cardNumber}
                   autoComplete="cc-number"
                   className="block w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black sm:text-sm"
                 />
               </div>
+              {state?.errors?.cardNumber && <p className="text-xs text-red-500">{state?.errors?.cardNumber.length > 1 ? state?.errors?.cardNumber[1] : state?.errors?.cardNumber[0]}</p>}
             </div>
 
             <div className="col-span-8 sm:col-span-9">
@@ -79,10 +69,12 @@ export default function Form() {
                   id="expiration-date"
                   name="expiration-date"
                   type="text"
+                  defaultValue={state?.inputs?.expirationDate}
                   autoComplete="cc-exp"
                   className="block w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black sm:text-sm"
                 />
               </div>
+              {state?.errors?.expirationDate && <p className="text-xs text-red-500">{state?.errors?.expirationDate.length > 1 ? state?.errors?.expirationDate[1] : state?.errors?.expirationDate[0]}</p>}
             </div>
 
             <div className="col-span-4 sm:col-span-3">
@@ -94,10 +86,12 @@ export default function Form() {
                   id="cvc"
                   name="cvc"
                   type="text"
+                  defaultValue={state?.inputs?.cvc}
                   autoComplete="csc"
                   className="block w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black sm:text-sm"
                 />
               </div>
+              {state?.errors?.cvc && <p className="text-xs text-red-500">{state?.errors?.cvc.length > 1 ? state?.errors?.cvc[1] : state?.errors?.cvc[0] }</p>}
             </div>
 
             <div className="col-span-full">
@@ -108,11 +102,13 @@ export default function Form() {
                 <input
                   id="address"
                   name="address"
+                  defaultValue={state?.inputs?.address}
                   type="text"
                   autoComplete="street-address"
                   className="block w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black sm:text-sm"
                 />
               </div>
+              {state?.errors?.address && <p className="text-xs text-red-500">{state?.errors?.address.length > 1 ? state?.errors?.address[1] : state?.errors?.address[0]}</p>}
             </div>
 
             <div className="col-span-full sm:col-span-9">
@@ -124,10 +120,12 @@ export default function Form() {
                   id="city"
                   name="city"
                   type="text"
+                  defaultValue={state?.inputs?.city}
                   autoComplete="address-level2"
                   className="block w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black sm:text-sm"
                 />
               </div>
+              {state?.errors?.city && <p className="text-xs text-red-500">{state?.errors?.city.length > 1 ? state?.errors?.city[1] : state?.errors?.city[0]}</p>}
             </div>
 
             <div className="col-span-full sm:col-span-3">
@@ -139,19 +137,16 @@ export default function Form() {
                   id="postal-code"
                   name="postal-code"
                   type="text"
+                  defaultValue={state?.inputs?.postalCode}
                   autoComplete="postal-code"
                   className="block w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black sm:text-sm"
                 />
               </div>
+              {state?.errors?.postalCode && <p className="text-xs text-red-500">{state?.errors?.postalCode.length > 1 ? state?.errors?.postalCode[1] : state?.errors?.postalCode[0]}</p>}
             </div>
           </div>
 
-          <button
-            type="submit"
-            className="mt-6 w-full rounded-md border border-transparent bg-black px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-black focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2"
-          >
-            Pay
-          </button>
+          <FormButton />
 
           <p className="mt-6 flex justify-center text-sm font-medium text-gray-500">
             <LockClosedIcon aria-hidden="true" className="mr-1.5 h-5 w-5 text-gray-400" />
