30efea4c9b519b76751901139c998c0018d6f477
Akram Idrissi <akram.mo.gb@gmail.com>
2025-01-12 21:30:12 +0100

    Updated middleware

--- src/middleware.js
+++ src/middleware.js
@@ -9,13 +9,11 @@ export function middleware(request) {
       return NextResponse.redirect(new URL('/', request.url));
     }
   } else if (pathname === '/checkout') {
-    if (!user) {
+    if (!user)
       return NextResponse.redirect(new URL(`/auth/signin`, request.url));
-    }
-    else if (pathname === '/orders') {
-      if (!user)
-        return NextResponse.redirect(new URL(`/`, request.url));
-    }
+  } else if (pathname === '/orders') {
+    if (!user)
+      return NextResponse.redirect(new URL(`/`, request.url));
   }
 
   return NextResponse.next();
