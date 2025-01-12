32dfaeb2c9062e158f08b969a356120ab70ec3af
Akram Idrissi <akram.mo.gb@gmail.com>
2025-01-12 22:06:40 +0100

    Reading from LS

--- src/components/navbar/navbar-wrapper.jsx
+++ src/components/navbar/navbar-wrapper.jsx
@@ -1,18 +1,32 @@
 "use client"
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 import DesktopNavbar from "./desktop-navbar";
 import MobileNavbar from "./mobile-navbar";
 
-export default function NavbarWrapper({isAuth, cartCount, navigation}) {
+export default function NavbarWrapper({ navigation }) {
+
+    const [auth, setAuth] = useState(true);
+    const [cartCount, setCartCount] = useState(0);
     const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
-    return(
+    useEffect(() => {
+        const user = document.cookie.split("; ").find(cookie => cookie.startsWith("user="));
+        
+        setAuth(user ? true : false);
+        setCartCount(parseInt(localStorage.getItem("products-counter")) || 0);
+
+        const handleStorageChange = () => { setCartCount(parseInt(localStorage.getItem("products-counter")) || 0); };
+        window.addEventListener("products-counter", handleStorageChange);
+        return () => window.removeEventListener("products-counter", handleStorageChange);
+    }, []);
+
+    return (
         <>
-            <DesktopNavbar isAuth={isAuth} cartCount={cartCount} navigation={navigation} setMobileMenuOpen={setMobileMenuOpen} />
-            <MobileNavbar isAuth={isAuth} cartCount={cartCount} navigation={navigation} mobileMenuOpen={mobileMenuOpen} setMobileMenuOpen={setMobileMenuOpen} />
+            <DesktopNavbar isAuth={auth} cartCount={cartCount} navigation={navigation} setMobileMenuOpen={setMobileMenuOpen} />
+            <MobileNavbar isAuth={auth} cartCount={cartCount} navigation={navigation} mobileMenuOpen={mobileMenuOpen} setMobileMenuOpen={setMobileMenuOpen} />
         </>
-    ); 
-    
+    );
+
 }
