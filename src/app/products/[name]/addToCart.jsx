c35111aa40b1d798f200db5d0d186a55ba0ebf02
Akram Idrissi <akram.mo.gb@gmail.com>
2025-01-17 21:47:47 +0100

    Using server actions

--- src/app/products/[name]/addToCart.jsx
+++ src/app/products/[name]/addToCart.jsx
@@ -1,19 +1,27 @@
-import { addToCart } from '@/app/cart/cart';
+import { addToCart } from '@/app/cart/actions';
 
 
-const handleCart = (event, product, selectedSize) => {
-  event.preventDefault();
-  let p = { 'id': product.id, 'href': `/products/${product.id}`, 'title': product.title, 'image': product.image_srcs[0], 'category': product.category, 'price': 145, 'size': selectedSize }
-  addToCart(product.id, p)
-};
+export default function AddToCart({ product, selectedSize = "S" }) {
 
-export default function AddToCart({ product, selectedSize="S" }) {
+  const addToCartWithProduct = async () => {
+    const cartItem = {
+      id: product.id,
+      href: `/products/${product.id}`,
+      title: product.title,
+      image: product.image_srcs[0],
+      category: product.category,
+      price: 145,
+      size: selectedSize,
+    };
+
+    await addToCart(product.id, cartItem);
+  };
+
 
   return (
-    <form className="mt-6">
+    <form action={() => addToCartWithProduct()} className="mt-6">
       <div className="mt-10 flex">
         <button
-          onClick={(event) => handleCart(event, product, selectedSize)}
           type="submit"
           className="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-black px-8 py-3 text-base font-medium text-white hover:bg-black focus:outline-none focus:ring-2 focus:ring-black focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full"
         >
