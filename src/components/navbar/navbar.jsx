8a47270e155a2f2e636551d38c78c2a1e00cc6cc
Akram Idrissi <akram.mo.gb@gmail.com>
2025-01-17 21:11:45 +0100

    Safety check

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -11,8 +11,8 @@ const navigation = [
 
 export default function Navbar({ sticky = false }) {
     const cookieStore = cookies();
-    const user = cookieStore.get("user").value || null;
-    const cartCount = cookieStore.get("products-counter").value || 0;
+    const user = cookieStore.get("user")?.value || null;
+    const cartCount = cookieStore.get("products-counter")?.value || 0;
     
     return (
         <header className={`${sticky && "sticky"} top-0 z-50 bg-white/60 backdrop-blur-lg`}>
